28d4c27eb179c4d415a6e4913adb82d350165b40
sakshi192000 <sakshisawants4100@gmail.com>
2021-03-01 12:16:55 +0530

    Navigation Section

---
+++ src/components/App/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./App.css";
+
+
+class App extends React.Component{
+    render(){
+        return(
+            <div className="app">
+                {/*Nav Section*/}
+                {/*Landing Section*/}
+                {/*About Section*/}
+                {/*Projects Section*/}
+                {/*Contact and Footer Section*/}
+
+
+            </div>
+        )
+    }
+}
+
+export default App;
