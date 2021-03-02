8936a8220cc02886c01ec89d01e1a1e671bad12f
sakshi192000 <sakshisawants4100@gmail.com>
2021-03-02 09:58:59 +0530

    Landing Page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./App.css";
 import Nav from "../Nav/Nav";
+import Landing from "../Landing/Landing"
 
 
 class App extends React.Component{
@@ -10,6 +11,7 @@ class App extends React.Component{
                 {/*Nav Section*/}
                 <Nav />
                 {/*Landing Section*/}
+                <Landing />
                 {/*About Section*/}
                 {/*Projects Section*/}
                 {/*Contact and Footer Section*/}
---
+++ src/components/Landing/Landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Landing.css';
+
+const Landing = () => {
+    return(
+        <div className="landing-container">
+            <h1 className="title">Sakshi Sawant</h1>
+
+
+        </div>
+    );
+};
+
+export default Landing;
