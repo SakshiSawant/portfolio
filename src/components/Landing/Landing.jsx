6200707da1de89b78806bdd92df80767eebb5f66
sakshi192000 <sakshisawants4100@gmail.com>
2021-03-02 11:00:33 +0530

    Navigation bar fixed

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -6,21 +6,23 @@ const Landing = () => {
         <div className="landing-container">
             <div className="title_block">
                 <h1 className="title">Sakshi Sawant</h1>
+                <h2 className="subtitle">I'm a</h2>
             </div>
-            <h1 className="subtitle">I'm a</h1>
             {/*
-            <div className="typewriter-container">
-                <Typewriter
-                    options={{
-                        strings: ['Student', 'Blogger', 'Web Developer'],
-                        autoStart: true,
-                        loop: true,
-                    }}
-                />
+                <div className="subtitle_block">
+                <h1 className="subtitle">I'm a</h1>
+                <div className="typewriter-container">
+                    <Typewriter
+                        options={{
+                            strings: ['Student', 'Blogger', 'Web Developer'],
+                            autoStart: true,
+                            loop: true,
+                        }}
+                    />
+                    </div>
                 </div>
                 */}
-
-
+                
 
         </div>
     );
