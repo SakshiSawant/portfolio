5d6f69d3b52c5784ba8274a3b3aa99e8fb142eca
sakshi192000 <sakshisawants4100@gmail.com>
2021-03-02 10:23:02 +0530

    Title Block fixed

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -4,7 +4,22 @@ import './Landing.css';
 const Landing = () => {
     return(
         <div className="landing-container">
-            <h1 className="title">Sakshi Sawant</h1>
+            <div className="title_block">
+                <h1 className="title">Sakshi Sawant</h1>
+            </div>
+            <h1 className="subtitle">I'm a</h1>
+            {/*
+            <div className="typewriter-container">
+                <Typewriter
+                    options={{
+                        strings: ['Student', 'Blogger', 'Web Developer'],
+                        autoStart: true,
+                        loop: true,
+                    }}
+                />
+                </div>
+                */}
+
 
 
         </div>
